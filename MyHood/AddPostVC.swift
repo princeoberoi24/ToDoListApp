e760194b4b74695db9df17c39a19cdc52caeb1e4
princeoberoi24 <princeoberoi24@gmail.com>
2020-04-20 15:16:16 +0530

    added index.html and I modified test1.txt

---
+++ MyHood/AddPostVC.swift
@@ -0,0 +1,47 @@
+//
+//  AddPostVC.swift
+//  MyHood
+
+import UIKit
+
+class AddPostVC: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
+
+    @IBOutlet weak var titleField: UITextField!
+    @IBOutlet weak var postImg: UIImageView!
+    @IBOutlet weak var descField: UITextField!
+    
+    var imagePicker: UIImagePickerController!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        postImg.layer.cornerRadius = 120
+        imagePicker = UIImagePickerController()
+        imagePicker.delegate = self
+    }
+    
+    @IBAction func addPicBtnPressed(_ sender: UIButton) {
+        sender.setTitle("", for: .normal)
+        present(imagePicker, animated: true, completion: nil)
+    }
+    
+    @IBAction func cancelBtnPressed(_ sender: UIButton) {
+        dismiss(animated: true, completion: nil)
+    }
+    
+    @IBAction func makePostBtnPressed(_ sender: UIButton) {
+        
+        if let title = titleField.text, let desc = descField.text, let img = postImg.image {
+            let imgPath = DataService.instance.saveImageAndCreatePath(img)
+            let post = Post(imagePath: imgPath, title: title, description: desc)
+            DataService.instance.addPost(post: post)
+            dismiss(animated: true, completion: nil)
+        }
+    }
+    
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+        let selectedImage = info[.originalImage] as? UIImage
+        imagePicker.dismiss(animated: true, completion: nil)
+        postImg.image = selectedImage
+    }
+}
---
+++ MyHood/PostCell.swift
@@ -0,0 +1,24 @@
+//
+//  PostCell.swift
+//  MyHood
+
+import UIKit
+
+class PostCell: UITableViewCell {
+
+    @IBOutlet weak var postImg: UIImageView!
+    @IBOutlet weak var titleLbl: UILabel!
+    @IBOutlet weak var descLbl: UILabel!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        postImg.layer.cornerRadius = 15
+    }
+    
+    func configureCell(_ post: Post) {
+        titleLbl.text = post.title
+        descLbl.text = post.postDesc
+        postImg.image = DataService.instance.imageForPath(post.imagePath)
+    }
+
+}
